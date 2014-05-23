e939b12ae5a5b79bd0024aec82f597578d779f2c
NikulinAndrei <nikulin.andrei@gmail.com>
2014-05-23 15:49:28 +0300

    Image resizing

--- app/services/FileService.scala
+++ app/services/FileService.scala
@@ -4,6 +4,9 @@ import java.io.File
 import scala.util.Random
 import play.api.mvc.MultipartFormData.FilePart
 import play.api.libs.Files.TemporaryFile
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
+import java.awt.Image
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,8 +20,35 @@ class FileService {
       throw new IllegalArgumentException( "Toetame ainult JPG formaadis pilte" )
     else{
       val filename = pk +"_"+System.currentTimeMillis()+"_"+ Random.nextInt()+".jpg"
-      file.ref.moveTo( new File("/tmp/images/"+filename))
+
+
+      val scaled = resizeImg( file.ref.file)
+      ImageIO.write( scaled , "png", new File("/tmp/images/"+filename) )
+
+      //file.ref.moveTo( new File("/tmp/images/"+filename))
       filename
     }
 
+  def resizeImg( f: File ) = {
+    val originalImage = ImageIO.read(f )
+
+    val scale = determineImageScale( originalImage.getWidth, originalImage.getHeight)
+
+    val scaled= originalImage.getScaledInstance(
+      (originalImage.getWidth * scale).toInt,
+      (originalImage.getHeight * scale).toInt,
+      Image.SCALE_SMOOTH)
+
+    val outImage = new BufferedImage(scaled.getWidth(null), scaled.getHeight(null), BufferedImage.TYPE_INT_RGB)
+
+    outImage.getGraphics().drawImage(scaled, 0, 0, null)
+
+    outImage
+  }
+
+  def determineImageScale( sourceWidth: Int, sourceHeight:Int ): Double = {
+    val scalex  = 640.toDouble / sourceWidth
+    val scaley = 480.toDouble / sourceHeight
+    Math.min(scalex, scaley)
+  }
 }
